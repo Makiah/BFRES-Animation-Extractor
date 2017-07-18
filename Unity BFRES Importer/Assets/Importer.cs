e758da67afc34c098cfecc388c5f27a2649f0c5e
Makiah Bennett <makiahtbennett@gmail.com>
2017-07-18 18:44:37 -0400

    ResFiles are being loaded successfully!!!!!!!

--- Unity BFRES Importer/Assets/Importer.cs
+++ Unity BFRES Importer/Assets/Importer.cs
@@ -1,22 +1,50 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using Syroot.NintenTools.Bfres;
 using Syroot.NintenTools.Yaz0;
 using UnityEngine;
 
-public class Importer : MonoBehaviour 
-{
+public class Importer : MonoBehaviour
+{	
 	// Use this for initialization
 	void Start ()
 	{
-		Yaz0Compression.Decompress("/Users/makiah/Desktop/Animal_Bass.sbfres", "/Users/makiah/Desktop/Animal_Bass.bfres");
-		Debug.Log("Should have decompressed!");
+		//Determine the paths of each of the components for the extraction.  
+		string sbfresFolder = Path.Combine(Application.dataPath, "SBFRES");
+		string sbfresFile = Path.Combine(sbfresFolder, "Animal_Bass.sbfres");
+		string bfresFile = Path.Combine(sbfresFolder, "Animal_Bass.bfres");
+
+		//Make sure that the specified sbfresFile path exists.  
+		if (!File.Exists(sbfresFile))
+		{
+			Debug.Log("SBFRES file not found! :(");
+			return;
+		}
+
+		//Extract the SBFRES file to a loadable BFRES.  
+		if (!File.Exists(bfresFile))
+		{
+			Yaz0Compression.Decompress(sbfresFile, bfresFile);
+			Debug.Log("Extracted "  + sbfresFile + " to " + bfresFile);
+		}
+		else
+		{
+			Debug.Log(bfresFile + " already exists, not extracting.");
+		}
 		
-		
-	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-		
+		//Load the model into a ResFile instance.  
+		ResFile loadedFile;
+		try
+		{
+			loadedFile = new ResFile(bfresFile);
+		}
+		catch (Exception e)
+		{
+			Debug.Log("Error while attempting to load " + bfresFile + " into a ResFile instance: " + e);
+			return;
+		}
+		Debug.Log("Loaded " + loadedFile.Name + " successfully!");
 	}
 }
