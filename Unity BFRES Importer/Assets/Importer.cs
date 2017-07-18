705be53bd93aa0e139f8ffd20b73b7a172183f18
Makiah Bennett <makiahtbennett@gmail.com>
2017-07-18 19:11:10 -0400

    Beginning to access ResFile data.

--- Unity BFRES Importer/Assets/Importer.cs
+++ Unity BFRES Importer/Assets/Importer.cs
@@ -2,24 +2,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using Syroot.Maths;
 using Syroot.NintenTools.Bfres;
 using Syroot.NintenTools.Yaz0;
+using UnityEditor;
 using UnityEngine;
 
 public class Importer : MonoBehaviour
-{	
+{
+	[SerializeField] private string sbfresToLoad;
+
+	public UnityEngine.Vector3 Vector3FtoVector3(Vector3F vector3f)
+	{
+		return new UnityEngine.Vector3(vector3f.X, vector3f.Y, vector3f.Z);
+	}
+
+	private void Start()
+	{
+		ImportSBFRES();
+	}
+	
 	// Use this for initialization
-	void Start ()
+	public void ImportSBFRES ()
 	{
 		//Determine the paths of each of the components for the extraction.  
 		string sbfresFolder = Path.Combine(Application.dataPath, "SBFRES");
-		string sbfresFile = Path.Combine(sbfresFolder, "Animal_Bass.sbfres");
+		string sbfresFile = Path.Combine(sbfresFolder, sbfresToLoad);
 		string bfresFile = Path.Combine(sbfresFolder, "Animal_Bass.bfres");
 
 		//Make sure that the specified sbfresFile path exists.  
-		if (!File.Exists(sbfresFile))
+		if (!File.Exists(sbfresFile) || Path.GetExtension(sbfresFile).Equals("sbfres"))
 		{
-			Debug.Log("SBFRES file not found! :(");
+			Debug.Log(sbfresFile + " was invalid!");
 			return;
 		}
 
@@ -46,5 +60,32 @@ public class Importer : MonoBehaviour
 			return;
 		}
 		Debug.Log("Loaded " + loadedFile.Name + " successfully!");
+		
+		//Now begin to load model components.  
+		foreach (var model in loadedFile.Models.Values)
+		{
+			Debug.Log("Now loading " + model.Name);
+
+			foreach (var bone in model.Skeleton.Bones.Values)
+			{
+				var currentBone = new GameObject(bone.Name + " " + bone.ParentIndex);
+				currentBone.transform.localPosition = Vector3FtoVector3(bone.Position);
+			}
+		}
+	}
+}
+
+[CustomEditor(typeof(Importer))]
+public class CustomEditorForSBFRESImporter : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector();
+        
+		var importer = (Importer)target;
+		if(GUILayout.Button("Import SBFRES"))
+		{
+			importer.ImportSBFRES();
+		}
 	}
 }
