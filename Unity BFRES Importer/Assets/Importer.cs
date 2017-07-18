a775bcfa9a0c07f18e967dbb897db8589c83d492
Makiah Bennett <makiahtbennett@gmail.com>
2017-07-18 18:29:17 -0400

    HOLY CRAP IT JUST EXTRACTED A FILE WITHOUT CRASHING

--- Unity BFRES Importer/Assets/Importer.cs
+++ Unity BFRES Importer/Assets/Importer.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Syroot.NintenTools.Yaz0;
 using UnityEngine;
 
-public class Importer : MonoBehaviour {
-
+public class Importer : MonoBehaviour 
+{
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
+		Yaz0Compression.Decompress("/Users/makiah/Desktop/Animal_Bass.sbfres", "/Users/makiah/Desktop/Animal_Bass.bfres");
+		Debug.Log("Should have decompressed!");
+		
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 		
 	}
 }
